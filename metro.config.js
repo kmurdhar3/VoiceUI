e01ee0f49f590ec0a2870cdf8e4f16fa10bc3399
Name <murdhariya.kamlesh3@gmail.com>
2026-03-05 16:59:32 +0000

    change of local fixes

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,16 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Add ngrok-skip-browser-warning header to bypass Ngrok's free-tier
+// interstitial page which causes "Cannot read properties of undefined (reading 'body')" errors
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      req.headers["ngrok-skip-browser-warning"] = "true";
+      return middleware(req, res, next);
+    };
+  },
+};
+
 module.exports = withNativeWind(config, { input: "./global.css" });
